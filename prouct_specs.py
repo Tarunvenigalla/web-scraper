01c6822de89c496616f1c40ff945dbcacb7e54d5
Tarunvenigalla <140583320+Tarunvenigalla@users.noreply.github.com>
2023-07-28 12:13:48 +0530

    updated

--- prouct_specs.py
+++ prouct_specs.py
@@ -1,25 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
 
-url=(input('Enter URL :'))
-response=requests.get(url)
-html=response.content
-soup=BeautifulSoup(html,"html.parser")
-brand=soup.find('tr',attrs={'class':'a-spacing-small po-brand'}).find('td',attrs={'class':'a-span9'}).text
-model=soup.find('tr',attrs={'class':'a-spacing-small po-model_name'}).find('td',attrs={'class':'a-span9'}).text
-screen_size=soup.find('tr',attrs={'class':'a-spacing-small po-display.size'}).find('td',attrs={'class':'a-span9'}).text
-color=soup.find('tr',attrs={'class':'a-spacing-small po-color'}).find('td',attrs={'class':'a-span9'}).text
-hard_disk=soup.find('tr',attrs={'class':'a-spacing-small po-hard_disk.size'}).find('td',attrs={'class':'a-span9'}).text
-cpu_model=soup.find('tr',attrs={'class':'a-spacing-small po-cpu_model.family'}).find('td',attrs={'class':'a-span9'}).text
-ram=soup.find('tr',attrs={'class':'a-spacing-small po-ram_memory.installed_size'}).find('td',attrs={'class':'a-span9'}).text
-os=soup.find('tr',attrs={'class':'a-spacing-small po-operating_system'}).find('td',attrs={'class':'a-span9'}).text
-price=soup.find('span',attrs={'class':'a-offscreen'}).text
-print('Brand :',brand)
-print('Model Name :',model)
-print('Screen Size :',screen_size)
-print('Color :',color)
-print('Hard Disk :',hard_disk)
-print('CPU Model :',cpu_model)
-print('RAM Size :',ram)
-print('Operating System :',os)
-print('Price :',price)
+def product_scrap(url):
+    try:
+        response=requests.get(url)
+        if response.status_code!=200:
+            print(f'Error: Failed to fetch web page (status code:{response.status_code})')
+            return None
+        soup=BeautifulSoup(response.content,"html.parser")
+        brand=soup.find('h1',class_='yhB1nd').find('span',class_='B_NuCI').text
+        price=soup.find('div',class_='_25b18c').find('div',class_='_30jeq3 _16Jk6d').text
+        rating=soup.find('div',class_='_3LWZlK').text
+        print('Product Details:',brand)
+        print('Product Price  :',price)
+        print('Product Rating :',rating)
+       
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
+
+target=input('Enter URL:')
+scrap=product_scrap(target)
+
+            
+            
+            
